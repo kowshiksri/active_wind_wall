b1b82c8971eae607d9cc31f20c53a7b14b611598
kowshik96 <kowshiksri@gmail.com>
2026-01-29 18:11:39 +0100

    test_pwm

--- test_pwm.py
+++ test_pwm.py
@@ -1,12 +1,38 @@
-from gpiozero import PWMOutputDevice
-from time import sleep
+import gpiod
+import time
 
-# GPIO18 = physical pin 12
-pwm = PWMOutputDevice(18, frequency=1000)
+GPIO_LINE = 18        # BCM GPIO18
+FREQ = 1000           # 1 kHz
+DUTY = 0.5            # 50%
+DURATION = 10         # seconds
 
-print("PWM ON (1 kHz, 50% duty) for 10 seconds")
-pwm.value = 0.5   # 50% duty cycle
-sleep(10)
+period = 1.0 / FREQ
+t_on = period * DUTY
+t_off = period * (1 - DUTY)
 
-print("PWM OFF")
-pwm.off()
+# Open GPIO chip
+chip = gpiod.Chip("gpiochip0")
+line = chip.get_line(GPIO_LINE)
+
+# Request line as output
+line.request(
+    consumer="pwm-test",
+    type=gpiod.LINE_REQ_DIR_OUT,
+    default_vals=[0]
+)
+
+print("PWM running (1 kHz, 50%) for 10 seconds...")
+
+start = time.time()
+while time.time() - start < DURATION:
+    line.set_value(1)
+    time.sleep(t_on)
+    line.set_value(0)
+    time.sleep(t_off)
+
+# Cleanup
+line.set_value(0)
+line.release()
+chip.close()
+
+print("Done.")
